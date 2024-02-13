c6af7471d88d15a6f94e9632edb8a28870c702a1
Rahul <heyrahul327@gmail.com>
2024-02-13 13:37:35 +0530

    updated

--- apps/english-learning-app/src/pages/index.tsx
+++ apps/english-learning-app/src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <h1>This is learning app</h1>
+      <h1>This is learning app updated</h1>
     </>
   );
 }
