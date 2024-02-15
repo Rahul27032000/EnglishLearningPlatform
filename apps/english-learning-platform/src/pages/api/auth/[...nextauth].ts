2b2a68ceda566a6c433bc81fda20121c3c8f94d8
Rahul <heyrahul327@gmail.com>
2024-02-15 19:15:12 +0530

    cleared authentication bug for deployment

--- apps/english-learning-platform/src/pages/api/auth/[...nextauth].ts
+++ apps/english-learning-platform/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { Provider } from "next-auth/providers";
 
 import GoogleProvider from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
@@ -17,46 +16,7 @@ export const authOptions = {
       clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET,
     }),
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      type: "credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        if (!credentials) {
-          return null;
-        }
-        const username = credentials.username;
-        const password = credentials.password;
-        // Add logic here to look up the user from the credentials supplied
-        const admin = await Admin.findOne({ username });
-
-        if (!admin) {
-          const obj = { username: username, password: password };
-          const newAdmin = new Admin(obj);
-          let adminDb = await newAdmin.save();
-          console.log(adminDb);
-          return {
-            id: adminDb._id,
-            email: adminDb.username,
-          };
-        } else {
-          //TODO:: Make this safer, encrypt passwords
-          if (admin.password !== password) {
-            return null;
-          }
-          // User is authenticated
-          return {
-            id: admin._id,
-            email: admin.username,
-          };
-        }
-      },
-    }),
-  ] as Provider[],
+  ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
